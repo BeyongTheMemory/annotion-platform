c3f8a75ee76e8e97a595dd786e2577adbf1a227c
Gang Xu <gang.xu@shopee.com>
2019-07-17 23:28:32 +0800

    commit

--- src/js/RelationExtractionItem.js
+++ src/js/RelationExtractionItem.js
@@ -281,7 +281,7 @@ class RelationExtractionItem extends Component {
                            prefix={<Icon type="fire" style={{color: 'rgba(0,0,0,.25)'}}/>}
                            placeholder="Input your comment here..."
                     />
-                    <Button style={{marginLeft: 50,backgroundColor:'rgba(115,86,50,.25)'}} onClick={this.onSubmit}><fron color="black">Submit</fron></Button>
+                    <Button style={{marginLeft: 50,backgroundColor:'rgba(115,86,50,.25)'}} onClick={this.onSubmit}><strong><fron color="black">Submit</fron></strong></Button>
                 </div>
 
                 <Modal
