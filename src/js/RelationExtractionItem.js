928e7a855dc87e8f56ea7719a23402e3b036b138
Gang Xu <gang.xu@shopee.com>
2019-07-25 22:11:46 +0800

    111

--- src/js/RelationExtractionItem.js
+++ src/js/RelationExtractionItem.js
@@ -271,7 +271,7 @@ class RelationExtractionItem extends Component {
                 </div>
 
 
-                <div style={{marginTop:200,width: '60%'}}>
+                <div style={{marginTop:50,width: '60%'}}>
                     <List
                         size="large"
                         dataSource={this.state.listData}
