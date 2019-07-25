1ad5ea351fbbc19755c2936fcc2d1f742158ff7f
Gang Xu <gang.xu@shopee.com>
2019-07-26 00:16:45 +0800

    111

--- src/js/RelationExtractionItem.js
+++ src/js/RelationExtractionItem.js
@@ -303,7 +303,7 @@ class RelationExtractionItem extends Component {
                         className="comment-input"
                         size='large' 
                         style={{width:'100%',height:50, fontSize:30,}}
-                        prefix={<Icon size="large" type="fire" style={{color: 'rgba(0,0,0,.25)',marginRight:15}}/>}
+                        prefix={<Icon size="large" type="fire" style={{color: 'rgba(0,0,0,.25)',marginRight:50}}/>}
                         placeholder="Input your comment here..."
                         onChange={(value) => {
                             this.handleCommentChange(value)
